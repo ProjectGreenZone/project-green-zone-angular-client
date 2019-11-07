37dfd02879caa3c23af5421ff8046e36a5be59e0
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-08 00:22:29 +0530

    deploying

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ng build --outputPath=./build --base-href /gz/ --deploy-url /gz/
+ng build --prod=true --outputPath=./build --base-href /gz/ --deploy-url /gz/
 git add *
 git commit -m "deploying"
 git push
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://52.163.53.231:9080',
+  ws_url: 'http://52.163.53.231:9080',
 };
