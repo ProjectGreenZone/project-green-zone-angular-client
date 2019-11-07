67393ab99d51748d97a069c4a20ebfc41c6894c9
Pasindu Perera <pasindubawantha@gmail.com>
2019-11-08 01:13:20 +0530

    deploying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://52.163.53.231:9080',
-  ws_url: 'http://52.163.53.231:9080',
+  url: 'https://dexternet.southeastasia.cloudapp.azure.com:9080',
+  ws_url: 'https://dexternet.southeastasia.cloudapp.azure.com:9080',
 };
